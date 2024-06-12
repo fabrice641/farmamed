a6140deb21ead66f7ba5e24a0bfe8265dc3f9dc6
fabrice641 <166943086+fabrice641@users.noreply.github.com>
2024-06-12 11:56:51 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,40 @@
+let nextButton = document.getElementById('next')
+let prevButton = document.getElementById('prev')
+let list = [].slice.call(document.querySelector('.container').children)
+
+function findActiveList() {
+  let activeList = list.findIndex((e) => {
+    return e.classList.contains('active')
+  })
+  
+  list[activeList].classList.remove('active', 'fadeInRight', 'fadeInLext', 'animated')
+  
+  return activeList
+}
+
+function slideShow() {
+  let activeList = findActiveList()
+  
+  activeList++
+  activeList = activeList === list.length ? 0 : activeList
+  
+  list[activeList].classList.add('active', 'fadeInRight', 'animated')
+}
+
+setInterval(slideShow, 5000)
+
+nextButton.addEventListener('click', slideShow)
+
+prevButton.addEventListener('click', () => {
+  let activeList = findActiveList()
+  
+  activeList--
+  activeList = activeList === -1 ? list.length - 1 : activeList
+  
+  list[activeList].classList.add('active', 'fadeInLeft', 'animated')
+})
+
+
 
         $(document).ready(function(){
             $(".on").click(function() {
