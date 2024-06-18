70d6ccbfab40060609bf390fb2914715359f8017
fabrice641 <166943086+fabrice641@users.noreply.github.com>
2024-06-18 13:43:05 +0200

    Add files via upload

---
+++ framework.js
@@ -0,0 +1,18 @@
+import 'jquery-ui.min.js';
+import 'bootstrap.min.css';
+import 'jquery-ui-theme.min.css';
+import 'bootstrap.min.js';
+import 'jquery-ui.structure.min.css';
+import 'bootstrap.bundle.js';
+import 'bootstrap.bundle.js.map';
+import 'bootstrap.bundle.min.js';
+import 'bootstrap.bundle.min.js.map';
+import 'bootstrap.esm.min.js';
+import 'bootstrap.esm.min.js.map';
+import 'bootstrap.js';
+import 'jquery-ui.js';
+import 'jquery-ui.min.js';
+import 'jquery-ui.structure.min.css';
+import 'jquery-ui.theme.min.css';
+
+
